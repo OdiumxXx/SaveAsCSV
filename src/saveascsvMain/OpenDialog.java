c480b74d4c638ced1b510d4ebbddc3ba13c45e47
OdiumxXx <cam.ofc@gmail.com>
2018-04-24 14:32:57 +1000

    SaveAsCSV 0.02
    - Static filename changed to members.xls
    - Added Basic GUI to select XLS and create CSV from OpenDialog
    - Included SaveAsCSV.jar (0.02) for download

---
+++ src/saveascsvMain/OpenDialog.java
@@ -0,0 +1,101 @@
+package saveascsvMain;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
+public class OpenDialog extends JDialog {
+  JLabel Label = new JLabel();  
+  String filePath;
+  private final JPanel contentPanel = new JPanel();
+
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    try {
+      OpenDialog dialog = new OpenDialog();
+      dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+      dialog.setVisible(true);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Create the dialog.
+   */
+  public OpenDialog() {
+    setBounds(100, 100, 450, 300);
+    getContentPane().setLayout(new BorderLayout());
+    contentPanel.setLayout(new FlowLayout());
+    contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+    getContentPane().add(contentPanel, BorderLayout.CENTER);
+    {
+      contentPanel.add(Label);      
+    }
+    {
+      JPanel buttonPane = new JPanel();
+      buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+      getContentPane().add(buttonPane, BorderLayout.SOUTH);
+      {
+        JButton okButton = new JButton("Select Spreadsheet");
+        okButton.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent arg0) {
+            JFileChooser fileChooser = new JFileChooser();
+            File selectedFile = null;
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+            int result = fileChooser.showOpenDialog(okButton);
+            if (result == JFileChooser.APPROVE_OPTION) {
+              selectedFile = fileChooser.getSelectedFile();
+              filePath = selectedFile.getAbsolutePath().toString();
+              System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+              Label.setText("Selected file: " + selectedFile.getAbsolutePath());
+            }
+
+          }
+        });
+        okButton.setActionCommand("OK");
+
+        buttonPane.add(okButton);
+        getRootPane().setDefaultButton(okButton);
+      }
+      {
+        JButton createCSVButton = new JButton("Create CSV File");
+        createCSVButton.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent arg0) {
+            saveascsvMain.ApachePOIExcelRead.main(filePath);
+            setVisible(false);
+          }
+        });
+        createCSVButton.setActionCommand("Create CSV File");        
+        buttonPane.add(createCSVButton);
+      }
+    }
+  }
+
+  public File fileChooser() {
+    JFileChooser fileChooser = new JFileChooser();
+    File selectedFile = null;
+    fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+    int result = fileChooser.showOpenDialog(this);
+    if (result == JFileChooser.APPROVE_OPTION) {
+      selectedFile = fileChooser.getSelectedFile();
+      System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+    }
+
+    return selectedFile;
+  }
+
+
+}
