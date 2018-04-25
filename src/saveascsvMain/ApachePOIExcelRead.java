9212cc92d1ff5cbef1a031ab62caae0c9b349121
OdiumxXx <cam.ofc@gmail.com>
2018-04-25 18:20:56 +1000

    0.04
    - Increased applet size
    - Added textArea and gave it some output
    - Added the 'close' button
    - General improvements

--- src/saveascsvMain/ApachePOIExcelRead.java
+++ src/saveascsvMain/ApachePOIExcelRead.java
@@ -12,78 +12,96 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ApachePOIExcelRead {
-  //  private static final String FILE_NAME = "members.xls"; 
-
+  //private static final String FILE_NAME = "members.xls"; 
   private static int ourSheet = 75;
 
+  // Our Text Area Editor (So we don't have to keep referring to saveascsvMain.etc.etc.etc) 
+  private static void textAreaEditor(String s) {
+    saveascsvMain.OpenDialog.textArea.append(s);
+  }
+
+  // Start the Conversion!
   public static void main(String filePath) {
 
     try {
 
+      if (filePath == null) {
+        textAreaEditor("\n  ERROR: You have not selected a spreadsheet.\n");
+      }
       FileInputStream excelFile = new FileInputStream(new File(filePath));
       Workbook workbook = new XSSFWorkbook(excelFile);      
       int sheetsNumberOf = workbook.getNumberOfSheets();  
 
-      //Setup regex to find the sheet we want to work with
+      //Setup regex to find the spreadsheet we want to work with
       Pattern p = Pattern.compile("Consolidate");   // the pattern to search for
-
-      // for each sheet, search for the regex pattern 'p' if the matcher makes a find of that pattern, set that to be oursheet and tell everyone about it!
+      System.out.println("- Checking all sheets in workbook for one with 'Consolidate' in the title....");
+      textAreaEditor("- Checking all sheets in workbook for one with 'Consolidate' in the title....\n");
+      // for each sheet, search for the regex pattern 'p' if the matcher makes a find of that pattern, set that to be 'ourSheet' and tell everyone about it!
       for(int i = 0; i < sheetsNumberOf; i++) {   
         String sheetName = workbook.getSheetName(i);
         Matcher m = p.matcher(sheetName);  
         if (m.find()) {
-          System.out.println("FOUND SHEET CONSOLIDATE!");
+          System.out.println("    Found Spreadsheet with phrase 'Consolidate'");
+          textAreaEditor("    Found Spreadsheet with phrase 'Consolidate'\n");
           ourSheet = i;
         }  else {
-          System.out.println("Not this sheet");
+          System.out.println("    Checking sheet "+i+"...");
+          textAreaEditor("    Checking sheet "+i+"...\n");
         }
       }
 
 
-
-
-
-
+      // Now we have the spreadsheet to work with, it's time to iterate through it and create our CSV
       Sheet datatypeSheet = workbook.getSheetAt(ourSheet);
       Iterator<Row> iterator = datatypeSheet.iterator();
-
       PrintWriter pw = new PrintWriter(new File("memdraw.csv"));
       StringBuilder sb = new StringBuilder();
+      int Entries = 0;
+      textAreaEditor("- Consolidate Members Sheet found!\n- Creating CSV...\n---------------------------------------\n");
 
       while (iterator.hasNext()) {
-
         Row currentRow = iterator.next();
         Iterator<Cell> cellIterator = currentRow.iterator();
 
         while (cellIterator.hasNext()) {
-
           Cell currentCell = cellIterator.next();
           //getCellTypeEnum deprecated for version 3.15
           //getCellTypeEnum will be renamed to getCellType in version 4.0
           if (currentCell.getCellTypeEnum() == CellType.STRING) {
             String cellValue = currentCell.getStringCellValue();
             System.out.print(cellValue + ","); 
+            textAreaEditor(cellValue + ",");            
             if (cellValue.contains("\n") == true) {
               cellValue = "Cell Value Contains NEWLINE - Fix your Spreadsheet!";
               System.out.print("*** REPLACED A NEW LINE ***");
+              textAreaEditor("\n*** WARNING: Cell Value Contains a linebreak. I have removed it for the CSV file, but you should fix your Spreadsheet! ***\n");
             }
-            sb.append(cellValue + ",");
+            sb.append(cellValue + ",");            
           } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {                        
             int cellValueNum = (int) currentCell.getNumericCellValue();
             System.out.print(cellValueNum + ",");
-            sb.append(cellValueNum + ",");
-          }
-
+            textAreaEditor(cellValueNum + ",");
+            sb.append(cellValueNum + ",");            
+          }          
         }
-        //                System.out.println("NEWLINE " + datatypeSheet.getLastRowNum());
-        System.out.println("");  
+        // END ROW
+        Entries++;         
+        textAreaEditor("\n");
         sb.append("\n");
-
-
       }
+      // END CSV
       pw.write(sb.toString());
       pw.close();
       workbook.close();
+
+      // ADDITIONAL APPLET OUTPUT
+      Entries--;
+      textAreaEditor("------------------------------\n");
+      textAreaEditor("- Succesfully created memdraw.csv!\n");      
+      textAreaEditor("- Members added: "+Entries+"\n");
+      textAreaEditor("\n");
+      textAreaEditor("Done!\n");
+      textAreaEditor("You may now close this window\n");
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     } catch (IOException e) {
--- src/saveascsvMain/OpenDialog.java
+++ src/saveascsvMain/OpenDialog.java
@@ -9,15 +9,29 @@ import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.Box;
 
 @SuppressWarnings("serial")
 public class OpenDialog extends JDialog {
-  JLabel Label = new JLabel();  
+  JLabel Label = new JLabel(); 
+  static JTextArea textArea = new JTextArea();
+  static JScrollPane sp = new JScrollPane(textArea);
   String filePath;
   private final JPanel contentPanel = new JPanel();
+  private final JButton btnExit = new JButton("Close");
+  private final Component horizontalStrut = Box.createHorizontalStrut(525);
+
+  private static void updateTextArea(String insertText) {
+    textArea.append(insertText);
+  }
 
   /**
    * Launch the application.
@@ -36,18 +50,27 @@ public class OpenDialog extends JDialog {
    * Create the dialog.
    */
   public OpenDialog() {
-    setBounds(100, 100, 450, 300);
+    setBounds(400, 400, 900, 600);
     getContentPane().setLayout(new BorderLayout());
-    contentPanel.setLayout(new FlowLayout());
     contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
     getContentPane().add(contentPanel, BorderLayout.CENTER);
+    contentPanel.setLayout(new BorderLayout(0, 0));
     {
-      contentPanel.add(Label);
-      Label.setText("Click 'Select Spreadsheet' and select the members database");
+      Label.setHorizontalAlignment(SwingConstants.CENTER);
+      contentPanel.add(Label, BorderLayout.NORTH);
+      Label.setText("<html>Click 'Select Spreadsheet' and select the members database<p>&nbsp<p /><p>&nbsp<p /></html>");
     }
     {
+      textArea.setBackground(Color.WHITE);
+      textArea.setWrapStyleWord(true);
+      textArea.setEditable(false);
+      contentPanel.add(sp, BorderLayout.CENTER);
+    }
+
+
+    {
       JPanel buttonPane = new JPanel();
-      buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+      buttonPane.setLayout(new FlowLayout(FlowLayout.LEFT));
       getContentPane().add(buttonPane, BorderLayout.SOUTH);
       {
         JButton okButton = new JButton("Select Spreadsheet");
@@ -61,7 +84,8 @@ public class OpenDialog extends JDialog {
               selectedFile = fileChooser.getSelectedFile();
               filePath = selectedFile.getAbsolutePath().toString();
               System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-              Label.setText("<html>Selected file:<br />"+ selectedFile.getAbsolutePath()+"<p>&nbsp</p><p>&nbsp</p>Click 'Create CSV File' to create the CSV file.<p>&nbsp<p />This window will close and a new 'memdraw.csv' will be created for you<br />in the same folder you opened this application from.<p>&nbsp</p>");
+              textArea.setText("\n- Selected file: " + selectedFile.getAbsolutePath()+"\n\n");
+              updateTextArea("Click 'Create CSV File' to create the CSV file.\nA new 'memdraw.csv' will be created for you in the same folder you launched this application from.\n\n");
             }
 
           }
@@ -76,11 +100,21 @@ public class OpenDialog extends JDialog {
         createCSVButton.addActionListener(new ActionListener() {
           public void actionPerformed(ActionEvent arg0) {
             saveascsvMain.ApachePOIExcelRead.main(filePath);
-            setVisible(false);
           }
         });
         createCSVButton.setActionCommand("Create CSV File");        
         buttonPane.add(createCSVButton);
+      }
+      {
+        buttonPane.add(horizontalStrut);
+      }
+      {
+        btnExit.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent arg0) {
+            System.exit(0);
+          }
+        });
+        buttonPane.add(btnExit);
       }
     }
   }
